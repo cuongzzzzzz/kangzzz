2a70a4b0a04dd89fb503e4933d4086e976d86081
kangz <bkak60.com>
2025-09-14 17:03:23 +0700

    change url get wordpress cli

--- practice/wordpress/deploy/docker-deploy.sh
+++ practice/wordpress/deploy/docker-deploy.sh
@@ -434,7 +434,8 @@ complete_wordpress_setup() {
         --admin_user="$WP_ADMIN_USER" \
         --admin_password="$WP_ADMIN_PASS" \
         --admin_email="$WP_ADMIN_EMAIL" \
-        --skip-email
+        --skip-email \
+        --allow-root
     
     print_status "WordPress setup completed"
 }
